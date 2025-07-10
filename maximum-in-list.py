8d844402e15e609008c5533a3e1d763f1beaa372
Noor Wahdain <noor@bootfi.com>
2025-07-10 13:58:44 +0300

    Maximum Number in List
    I started solving the 'maximum number in a list' problem by defining the algorithm of the solution

---
+++ maximum-in-list.py
@@ -0,0 +1,14 @@
+"""
+Get the largest number in a list
+1. Generate a random list from 1 - 100,000, has 20 elements, and call it 'lst'
+2. Create an integer varialbe 'maximum' to represent the maximum number in the list
+3. initiate 'maximum' with the first value in the list
+4. Loop over the list while comparing each item with 'maximum':
+    4.1. if the current value in the list is greater than maximum:
+        4.1.1. Set 'maximum' to the current value
+    4.2 If the current value is less than or equal 'maximum':
+        4.2.1. Continue the loop
+    4.3. If the list has no more values:
+        4.3.1. Stop the loop
+5. Display the maximum value in the console
+"""
