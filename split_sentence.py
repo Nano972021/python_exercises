02ac3a154c8e0cb05551a08bc5fe6ff256739874
Noor Wahdain <noor@bootfi.com>
2025-07-13 16:37:55 +0300

    Split Sentence Problem 1.0
    This is my first attempt to solve the problem. I encountered an issue, and that is the last word is added to the list without its last letter.
    
    Also, I changed the order on which I defined the 'word' variable to outside the loop. That is because previously, each time the 'start' value increases, the 'word' variable is emptied.

--- split_sentence.py
+++ split_sentence.py
@@ -8,22 +8,22 @@ Ver. 1.0
 Function Instructions:
 1. Create a new variable 'start', and initialize it with 0; it will represent the loop start value
 2. Create a variable 'lst', and initialize it as an empty list []; it will hold the sentence's words
-3. Loop over the sentence, as long as 'start' is less that the sentence's length:
-    3.1. Create a new variable called 'word', and initialize it as an empty space
-    3.1.If sentence[start] is an empty space:
-        3.1.1. If 'word' is empty - which means that no words have been added to the list:
-            3.1.1.1. Increase the 'start' varialbe by 1
-            3.1.1.2. Continue the loop
-        3.1.2. If 'word' is not empty - which means the first character was not a space:
-            3.1.2.1. Add the value of 'word' to 'lst'
-            3.1.2.2. Empty the variable 'word'
-            3.1.2.3. Increate the 'start' variable by one
-    3.2. If sentence[start] is not an empty space:
-        3.2.1. Add sentence[start] to 'word'
-        3.2.2. Increase the 'start' variable by 1
-    3.3. If 'start' equals sentence's length - 1:
-        3.3.1. Add the value of 'word' to 'lst'
-4. Return lst
+3. Create a new variable called 'word', and initialize it as an empty space
+4. Loop over the sentence, as long as 'start' is less that the sentence's length:
+    4.1.If sentence[start] is an empty space:
+        4.1.1. If 'word' is empty - which means that no words have been added to the list:
+            4.1.1.1. Increase the 'start' varialbe by 1
+            4.1.1.2. Continue the loop
+        4.1.2. If 'word' is not empty - which means the first character was not a space:
+            4.1.2.1. Add the value of 'word' to 'lst'
+            4.1.2.2. Empty the variable 'word'
+            4.1.2.3. Increate the 'start' variable by one
+    4.2. If sentence[start] is not an empty space:
+        4.2.1. Add sentence[start] to 'word'
+        4.2.2. Increase the 'start' variable by 1
+    4.3. If 'start' equals sentence's length - 1:
+        4.3.1. Add the value of 'word' to 'lst'
+5. Return lst
 ---------------------------------------------------------------------
 Test Data
 1. "I have a car"
@@ -32,4 +32,30 @@ Test Data
 4. " "
             
 """
+start = 0
+lst = []
+word = "" # Change No.01
+sentence = input("Enter a sentence: ")
+while start < len(sentence):
+    
+    if sentence[start] == " ":
+        
+        if word == "":
+            start += 1
+            continue
 
+        else:
+            lst.append(word) # When we reach the last word, the loop is already finished by then and we can't append the last word to the list
+            word = ""
+            start += 1
+
+    else:
+        word += sentence[start]
+        print(word)
+        start += 1
+
+
+    if start == len(sentence) - 1:
+        lst.append(word)
+
+print(lst)
