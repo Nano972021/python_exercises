0c9ab47f47d75bb909b0c673659bfde7459be6d2
Noor Wahdain <noor@bootfi.com>
2025-07-13 14:44:15 +0300

    Count Word Frequency
    In this file, I wrote the abstract steps to solve the 'count word frequency' problem.

---
+++ count_word_frequency.py
@@ -0,0 +1,7 @@
+"""
+Count word frequency in a sentence
+1. Split the sentence into its words
+2. Count the number of times a word has occured in the sentence
+
+Note: I'll use the modular programming design pattern
+"""
