ebb0fa853beeb3b6bb00496b4ec9afb0ee113142
Noor Wahdain <noor@bootfi.com>
2025-07-15 15:54:33 +0300

    Case Sensetivity Algorithm
    I wrote the initial idea of the solution.

---
+++ case_sensetivity.py
@@ -0,0 +1,18 @@
+"""
+Case Sensitivity
+Ver. 1.0
+1. Create a function called 'to_lowercase'
+2. The function takes 1 parameter called 'string', which represent the sting to convert to lowercase
+3. The function returns the string in lowercase
+------------------------------------------------------------------
+Function Instructions
+1. Loop over the string, character by character
+    1.1. Check if the character is in lowercase
+        1.1.1. If the ascii code of the current character is between 97 and 122:
+            1.1.1.1. Continue the loop
+        1.1.2. If the ascii code of the current character is between 65 and 90:
+            1.1.2.1. decrease the ascii code by 32
+
+2. Display the string
+        
+"""
