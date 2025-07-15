94a45c1f77a1b12419fb38a1dff2926d33f4f704
Noor Wahdain <noor@bootfi.com>
2025-07-15 15:53:11 +0300

    Split Sentence Problem 1.0
    I fixed some typos in the algorithm.

--- split_sentence.py
+++ split_sentence.py
@@ -2,17 +2,17 @@
 Split Sentence
 Ver. 1.0
 1. Create a function called 'split_sentence'
-2. The function takes 1 parameter called 'sentence', which represents the sentence to be split
-3. The function returns the lst of words in the sentence
+2. The function takes 1 parameter called 'sentence', which represent the sentence to be split
+3. The function returns the list of words in the sentence
 ---------------------------------------------------------------------
 Function Instructions:
 1. Create a new variable 'start', and initialize it with 0; it will represent the loop start value
 2. Create a variable 'lst', and initialize it as an empty list []; it will hold the sentence's words
 3. Create a new variable called 'word', and initialize it as an empty space
-4. Loop over the sentence, as long as 'start' is less that the sentence's length:
+4. Loop over the sentence, as long as 'start' is less than the sentence's length:
     4.1.If sentence[start] is an empty space:
         4.1.1. If 'word' is empty - which means that no words have been added to the list:
-            4.1.1.1. Increase the 'start' varialbe by 1
+            4.1.1.1. Increase the 'start' variable by 1
             4.1.1.2. Continue the loop
         4.1.2. If 'word' is not empty - which means the first character was not a space:
             4.1.2.1. Add the value of 'word' to 'lst'
@@ -21,8 +21,7 @@ Function Instructions:
     4.2. If sentence[start] is not an empty space:
         4.2.1. Add sentence[start] to 'word'
         4.2.2. Increase the 'start' variable by 1
-5. If 'start' equals sentence's length - 1:
-    5.1. Add the value of 'word' to 'lst'
+5. Add the value of 'word' to 'lst'
 6. Return lst
 ---------------------------------------------------------------------
 Test Data
